0bd3076fd128c6ba29a836ca57bea2f2aedc506f
Me Akash <158308964+MeAkash77@users.noreply.github.com>
2026-05-25 13:33:47 +0530

    Update config.py

--- examples/streamlit/config.py
+++ examples/streamlit/config.py
@@ -1,5 +1,5 @@
 # config.py
-SMTP_SERVER=itsaakash77@gmail.com
-SMTP_PORT=587
-SENDER_EMAIL=itsaakash77@gmail.com
-SENDER_PASSWORD=ndya huec xlgq mqpd
+SMTP_SERVER = "itsaakash77@gmail.com"
+SMTP_PORT = 587
+SENDER_EMAIL = "itsaakash77@gmail.com"
+SENDER_PASSWORD = "ndya huec xlgq mqpd"
